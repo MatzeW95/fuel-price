1a68ebec8902603e5086170fff72178767e315d1
Matthias Wagner <22793681+MatzeW95@users.noreply.github.com>
2023-01-18 21:12:43 +0100

    added getFuelData

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+import { apiKey } from "./apiKey.js";
+const key = apiKey.apiKey;
+
+
 window.onload = function () {
     console.log("onload");
 
@@ -6,7 +10,7 @@ window.onload = function () {
 
 function getLocation() {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
+      navigator.geolocation.getCurrentPosition(getFuelData);
     } else { 
       console.log("Geolocation is not supported by this browser.");
     }
@@ -15,4 +19,21 @@ function getLocation() {
 function showPosition(position) {
     console.log("Lat: " + position.coords.latitude + 
     " Long: " + position.coords.longitude);
+}
+
+function getFuelData(position) {
+
+    var url = "https://creativecommons.tankerkoenig.de/json/list.php?lat=" + position.coords.latitude + "&lng=" + position.coords.longitude + "&rad=5&sort=dist&type=all&apikey=" + key;
+
+    fetch(url, {
+        method: "get"
+    }).then(function (response) {
+        return response.text();
+    }).then(function (text) {
+        var data = JSON.parse(text);
+        //enter new function here
+        console.log(text);
+    }).catch(function (error) {
+        console.error(error);
+    });
 }
